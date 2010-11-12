8a87288ced0ab0383950583042cf83040b81a748
Dmitry Volkov <Dmitry.Volkov@me.com>
2010-11-12 19:12:55 +0300

    Renamed app delegate

---
+++ Classes/ThcAppDelegate.h
@@ -0,0 +1,36 @@
+//
+//  thc_ipadAppDelegate.h
+//  thc-ipad
+//
+//  Created by Dmitry Volkov on 11.11.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class RootViewController;
+
+@interface ThcAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+	
+	RootViewController *rootViewController;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet RootViewController *rootViewController;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
+
