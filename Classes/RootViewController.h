40a9086d5422ee6cb02a05af0283ff62d6dc2cf7
Dmitry Volkov <Dmitry.Volkov@me.com>
2010-11-12 19:12:45 +0300

    added RootViewController

---
+++ Classes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  thc-ipad
+//
+//  Created by Dmitry Volkov on 12.11.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Classes/RootViewController.m
@@ -0,0 +1,59 @@
+    //
+//  RootViewController.m
+//  thc-ipad
+//
+//  Created by Dmitry Volkov on 12.11.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+
+@implementation RootViewController
+
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+/*
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization.
+    }
+    return self;
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+*/
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Overriden to allow any orientation.
+    return YES;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc. that aren't in use.
+}
+
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
