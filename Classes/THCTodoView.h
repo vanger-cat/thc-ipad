4a3d1fdd875b9d75bdcf565b0054d1eb5ca2b85f
vanger-cat <katkov.anton@gmail.com>
2010-11-25 10:18:12 +0300

    added Todo View

---
+++ Classes/THCTodoView.h
@@ -0,0 +1,17 @@
+//
+//  THCTodoView.h
+//  thc-ipad
+//
+//  Created by Vanger on 24.11.10.
+//  Copyright 2010 Magic Ink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface THCTodoView : NSObject {
+	UISwitch *switch;
+	
+}
+
+@end
---
+++ Classes/THCTodoView.m
@@ -0,0 +1,14 @@
+//
+//  THCTodoView.m
+//  thc-ipad
+//
+//  Created by Vanger on 24.11.10.
+//  Copyright 2010 Magic Ink. All rights reserved.
+//
+
+#import "THCTodoView.h"
+
+
+@implementation THCTodoView
+
+@end
