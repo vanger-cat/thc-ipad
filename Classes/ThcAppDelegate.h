6950e63aff7f03cf2eb2d78882ec5978b110cab5
Dmitry Volkov <Dmitry.Volkov@me.com>
2010-11-25 12:10:04 +0300

    Added comment

--- Classes/ThcAppDelegate.h
+++ Classes/ThcAppDelegate.h
@@ -3,7 +3,7 @@
 //  thc-ipad
 //
 //  Created by Dmitry Volkov on 11.11.10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 Magik Ink. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
